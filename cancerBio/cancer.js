9f2dbfadf3d4296c7875d30a11902b0b37f56fe8
emanuelFernandes <emanuel_queiroga1@hotmail.com>
2016-01-09 15:21:13 +0000

    cancer

---
+++ cancerBio/cancer.js
@@ -0,0 +1,16 @@
+{
+  "nodes":[
+    {"name":"Myriel","group":1},
+    {"name":"Napoleon","group":2},
+    {"name":"Mlle.Baptistine","group":1}
+  ],
+  "nodes2":[
+    {"name":"Name1","group":1},
+    {"name":"Name2","group":2},
+    {"name":"Name3","group":1}
+  ],
+  "links":[
+    {"source":0,"target":1,"value":3}
+   
+  ]
+}
